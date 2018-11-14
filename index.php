de66abe58672798338a3729ad43252e4212aae22
Tom Bennett-Lloyd <tom664@hotmail.co.uk>
2018-11-14 00:12:46 +0000

    Initial Commit

--- index.php
+++ index.php
@@ -6,23 +6,12 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
-        <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-        <style>
-            body {
-                padding-top: 50px;
-                padding-bottom: 20px;
-            }
-        </style>
-        <link rel="stylesheet" href="css/bootstrap/bootstrap-theme.min.css">
+        <link href="external/Bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="external/Bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="css/main.css">
-
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
-        <!--[if lt IE 8]>
-            <p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
